b4db7cd2eb86fc0a13e4a7b485cb63bd3d99c69e
nour <nour.kachmar0@hotmail.com>
2024-02-24 01:39:07 +0200

    sign out works

--- screens/drawerScreens/Drawer.js
+++ screens/drawerScreens/Drawer.js
@@ -1,13 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, SafeAreaView, Text, View } from "react-native";
+import { Button, SafeAreaView, Text } from "react-native";
+import { FIREBASE_AUTH } from "../../firebaseConfig";
+import React from 'react';
 
 export default function Drawer() {
   const navigation = useNavigation();
+
   return (
     <SafeAreaView>
       <Text>test</Text>
       <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
-      <Button title="Go Back" onPress={() => navigation.goBack()} />
+      <Button title="Sign Out" onPress={() => FIREBASE_AUTH.signOut()} />
     </SafeAreaView>
   );
 }
